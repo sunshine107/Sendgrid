ae07566113672db6312dea8e0f191c10f7d0f082
alexchitu <alexchitu1991@outlook.com>
2018-05-22 14:24:23 +0300

    sdf

--- app.js
+++ app.js
@@ -20,6 +20,26 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 }); //Catch all other routes and return the index file
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.post('/myaction', function(req, res) {
     //res.send('You sent the name "' + req.body.name + '".');
     var email = req.body.email;
