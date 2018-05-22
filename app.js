e545a7d2bb648d73f841c68d74c5fbba1d7d4d02
alexchitu <alexchitu1991@outlook.com>
2018-05-22 15:55:49 +0300

    at last

--- app.js
+++ app.js
@@ -20,6 +20,25 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 }); //Catch all other routes and return the index file
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.post('/myaction', function(req, res) {
     //res.send('You sent the name "' + req.body.name + '".');
@@ -32,8 +51,7 @@ app.post('/myaction', function(req, res) {
       + "Thanks.";
    
    const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey("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");  //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
-
+    sgMail.setApiKey("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");
     const msg = {
       to: email,
       from: 'no-reply@htsactivealertsystems.com',
